3e2b7990273d69e3abae3bc218991cb87ec1bb0a
mahirabanushadakwala <mahirabanushadakwala@gmail.com>
2026-07-13 20:46:32 +0530

    Created landing page and dashboard for Student Result Management

---
+++ Student_Result_Management/app.py
@@ -0,0 +1,18 @@
+import webbrowser
+
+from flask import Flask, render_template
+import webbrowser
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+if __name__ == "__main__":
+    webbrowser.open("http://127.0.0.1:5000")
+    app.run(debug=True)
